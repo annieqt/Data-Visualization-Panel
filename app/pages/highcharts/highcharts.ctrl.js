4d0b4f6acb3cc666f0682bf17e0ccf773129b5d9
Tian Wang <t-wang14@mails.tsinghua.edu.cn>
2016-09-01 15:07:07 +0800

    Create highcharts.ctrl.js

---
+++ app/pages/highcharts/highcharts.ctrl.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Tian Wang on 9/1/2016.
+ */
+(function () {
+    'use strict';
+
+    angular.module('myApp.highcharts')
+        .controller('HighchartsCtrl', HighchartsCtrl);
+
+    /** @ngInject */
+    function HighchartsCtrl($scope, $timeout) {
+
+    }
+})();
