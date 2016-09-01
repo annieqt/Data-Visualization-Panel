210c083b65103d174297fb81c0edfd2b3656b2da
Tian Wang <t-wang14@mails.tsinghua.edu.cn>
2016-09-01 15:03:40 +0800

    Create d3.ctrl.js

---
+++ app/pages/d3/d3.ctrl.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Tian Wang on 9/1/2016.
+ */
+(function () {
+    'use strict';
+
+    angular.module('myApp.d3')
+        .controller('D3Ctrl', D3Ctrl);
+
+    D3Ctrl.$inject = ['$scope', '$rootScope', 'D3Service'];
+
+    /** @ngInject */
+    function D3Ctrl($scope, $rootScope, D3Service) {
+
+    }
+})();
